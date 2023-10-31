15d598365f33478c2401d1b6db405286987b73cd
momorichan <wonkj111@naver.com>
2023-10-31 15:43:43 +0900

    1

--- TeamDModule/src/teamD/module/mvc/controller/product/ProductController.java
+++ TeamDModule/src/teamD/module/mvc/controller/product/ProductController.java
@@ -2,7 +2,9 @@ package teamD.module.mvc.controller.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import teamD.module.mvc.dao.ProductDaoInter;
 import teamD.module.mvc.dto.ProductVO;
@@ -10,14 +12,12 @@ import teamD.module.mvc.dto.ProductVO;
 @Controller
 public class ProductController {
 	@Autowired
-	private ProductDaoInter ProductDaoInter;
+	private ProductDaoInter productDaoInter;
 	
 	
 	@GetMapping("/prInsert")
-	public String prInsert() {
+	public String prInsertForm(Model model) {
+		model.addAttribute("lclist", productDaoInter.lcList());
 		return "product/prInsertForm";
 	}
-	
-	
-
 }
--- TeamDModule/src/teamD/module/mvc/dao/ProductDao.java
+++ TeamDModule/src/teamD/module/mvc/dao/ProductDao.java
@@ -1,10 +1,13 @@
 package teamD.module.mvc.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import teamD.module.mvc.dto.LCategoryVO;
 import teamD.module.mvc.dto.ProductVO;
 
 @Repository
@@ -16,6 +19,11 @@ public class ProductDao implements ProductDaoInter{
 	@Override
 	public void prInsert(ProductVO vo) {
 		ss.insert("product.add",vo);
+	}
+
+	@Override
+	public List<LCategoryVO> lcList() {
+		return ss.selectList("product.lclist");
 	}
 
 }
